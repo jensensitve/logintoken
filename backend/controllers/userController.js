8a45e7e85f8b802efd0564174382dde67b46648b
jens neu <jensnewstarter@gmail.com>
2023-08-10 15:37:00 +0200

    userController change

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,8 +2,22 @@ import userModel from "../model/userModel.js";
 import bcrypt from 'bcrypt'
 import { createToken } from "../lib/auth.js";
 
-
 export async function createUserController(req, res) {
+    try {
+        const saltRounds = 12
+        const salt = await bcrypt.genSalt(saltRounds)
+        const hashedSaltedPassword = await bcrypt.hash(req.body.password, salt)
+        req.body.password = hashedSaltedPassword
+
+        const newUser = new userModel(req.body)
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+};
+
+/* export async function createUserController(req, res) {
     try {
         const saltRounds = 12
         const salt = await bcrypt.genSalt(saltRounds)
@@ -17,7 +31,7 @@ export async function createUserController(req, res) {
     } catch (err) {
         res.status(500).json(err)
     }
-}
+} */
 
 export async function loginUserController(req, res) {
     try {
