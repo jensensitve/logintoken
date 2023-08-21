88610defe44eba37df000b2f0a7c955baa236058
jens neu <jensnewstarter@gmail.com>
2023-08-21 11:32:18 +0200

    viteconfig

--- Frontend/loginFrontend/vite.config.js
+++ Frontend/loginFrontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: "https://logintoken.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
